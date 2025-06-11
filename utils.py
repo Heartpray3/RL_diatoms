7c07779eb68446f71e13135de44b9eda2740ab85
Ely Cheikh Abass <elycheikh2002@hotmail.com>
2025-06-11 16:37:18 +0200

    deleting refs to fourrier

--- utils.py
+++ utils.py
@@ -19,7 +19,7 @@ def load_config(path="config.yaml") -> Config:
     with open(path, 'r') as file:
         return Config(**yaml.safe_load(file))
 
-def abs_path(directory: str) -> Path:
+def abs_path(directory: str) -> str:
     script_dir = Path(__file__).parent.resolve()
     abs_dir = (script_dir / directory).resolve()
-    return abs_dir
+    return str(abs_dir)
