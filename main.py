1ad3f099abf7b747689458f5cfc4b4ae38abbae1
Ely Cheikh Abass <elycheikh2002@hotmail.com>
2025-06-11 15:26:47 +0200

    first config main script

---
+++ main.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from utils import load_config, abs_path
+from My_Generate_all_files_efficiency_optimization7 import main
+
+if __name__ == '__main__':
+    config = load_config()
+
+
+    main(
+        abs_path(config.output_directory),
+        config.Nblobs,
+        config.phase_shift,
+        config.Nrods,
+        config.an,
+        config.bn,
+        config.nmodes,
+        config.dt,
+        config.Nstep,
+        config.freq
+    )
