6b2ff0fc192489aa28136b9570b252f7b663968c
Ely Cheikh Abass <elycheikh2002@hotmail.com>
2025-06-13 18:38:21 +0200

    removing unused files, project starting to look like something

--- Test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-Nblobs = 14
-phase_shift = 2.0
-Nrods = 15
-
-my_command = f"python3 Generate_all_files_efficiency_optimization.py --Nblobs {Nblobs} --phase_shift {phase_shift} --Nrods {Nrods}"
-os.system(my_command)
