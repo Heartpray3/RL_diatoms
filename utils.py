3d2aba73e6259fd3c09a45406b15fd032a8a1570
Ely Cheikh Abass <elycheikh2002@hotmail.com>
2025-07-01 16:22:44 +0200

    refactor

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import yaml
 from dataclasses import dataclass
 from pathlib import Path
 import os
+import numpy as np
 
 from enum import Enum, auto
 
@@ -50,3 +51,20 @@ def get_sim_folder(output_dir: str, n_rods: int, n_blobs: int):
     parent_folder = os.path.join(blob_folder, rod_folder)
     output_folder = os.path.join(parent_folder, root_name + f"{n_blobs}_blobs_{n_rods}_rods")
     return os.path.join(output_dir, output_folder)
+
+def quaternion_rotation_matrix(q0, q1, q2, q3):
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+
+    return np.array([[r00, r01, r02],
+                     [r10, r11, r12],
+                     [r20, r21, r22]])
